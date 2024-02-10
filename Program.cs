921db26ec5324515415799e9f6983d3f55b088e2
Oliwia <106619574+okrzanowska@users.noreply.github.com>
2024-02-10 13:52:03 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Lenght
+{
+    class Program
+    {
+        private static void Main()
+        {
+            Console.Write("Podaj nazwę miasta: ");
+            var city = Console.ReadLine();
+
+            Console.Write("Podaj nazwę województwa: ");
+            var region = Console.ReadLine();
+
+            Console.WriteLine($"Nazwa miasta {city} zawiera {city?.Length} liter. Nazwa województwa {region} zawiera {region?.Length} liter.");
+        }
+    }
+}
